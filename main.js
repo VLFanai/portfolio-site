5b59e19da2c640fc7ce97319689692ca25b6b3c6
VLFanai <89696427+VLFanai@users.noreply.github.com>
2023-06-25 22:30:55 +0530

    Add files via upload
    Update 25.06.023

--- main.js
+++ main.js
@@ -1,32 +1,167 @@
-const mainTheme = document.querySelector('main');
-const toggleButton = document.querySelector('#dark-mode-toggle');
+// Add an event listener to the window load event
+window.addEventListener('load', function() {
+  // Get all the navigation links
+  var homeLink = document.getElementById('home-link');
+  var aboutLink = document.getElementById('about-link');
+  var blogLink = document.getElementById('blog-link');
+  var projectsLink = document.getElementById('projects-link');
+  var contactLink = document.getElementById('contact-link');
 
-function toggleTheme() {
-  mainTheme.classList.toggle("light-mode");
-}
+  // Get all the sections
+  var homeSection = document.getElementById('home-section');
+  var aboutSection = document.getElementById('about-section');
+  var blogSection = document.getElementById('blog-section');
+  var projectsSection = document.getElementById('projects-section');
+  var contactSection = document.getElementById('contact-section');
 
-  
+  // Set the initial section to display
+  homeSection.style.display = 'flex';
+  aboutSection.style.display = 'none';
+  blogSection.style.display = 'none';
+  projectsSection.style.display = 'none';
+  contactSection.style.display = 'none';
 
-// --TYPING ANIMATION
-// Delay in milliseconds before the text appears and starts typing
-const delay = 2000;
-
-// Function to initialize the typing animation after the delay
-function startTyping() {
-  var typed = new Typed('#auto-type2', {
-    strings: ['Developer', 'Youtuber', 'Illustrator'],
-    typeSpeed: 100,
-    backSpeed: 70,
-    loop: true,
+  // Add click event listeners to the navigation links
+  homeLink.addEventListener('click', function() {
+    showSection(homeSection);
   });
+
+  aboutLink.addEventListener('click', function() {
+    showSection(aboutSection);
+  });
+
+  blogLink.addEventListener('click', function() {
+    showSection(blogSection);
+  });
+
+  projectsLink.addEventListener('click', function() {
+    showSection(projectsSection);
+  });
+
+  contactLink.addEventListener('click', function() {
+    showSection(contactSection);
+  });
+
+  // Function to show a specific section and hide the rest
+  function showSection(section) {
+    // Hide all sections
+    homeSection.style.display = 'none';
+    aboutSection.style.display = 'none';
+    blogSection.style.display = 'none';
+    projectsSection.style.display = 'none';
+    contactSection.style.display = 'none';
+
+    // Show the selected section
+    section.style.display = 'flex';
+  }
+});
+
+
+// Define an array of blog posts
+const blogPosts = [
+  {
+    title: 'Elevating my Portrait Drawing Skills through AI-Generated Art',
+    description: '"Unlocking my artistic potential, I harness AI-generated art to enhance my portrait-drawing skills and explore boundless creativity."',
+    imagePath: 'images/thumbnails/illustration.png',
+    link: 'https://vl-fanai.blogspot.com/2023/06/elevating-my-portrait-drawing-skills.html'
+  },
+  {
+    title: 'Starting a Blog for Reflection and Accountability: Overcoming Procrastination and Embracing Imperfection',
+    description: '"This is another blog post description."',
+    imagePath: 'images/thumbnails/Man__dark-bg.jpeg',
+    link: 'https://vl-fanai.blogspot.com/2023/04/why-i-finally-decided-to-start-writing.html'
+  },
+  {
+    title: 'This is the third blog',
+    description: '"This is the third blog post description."',
+    imagePath: 'images/thumbnails/Man__dark-bg.jpeg',
+    link: 'https://vl-fanai.blogspot.com/2023/04/ai-will-not-replacr-your-job-people-who.html'
+  },
+  // Add more blog posts as needed
+];
+
+let currentPostIndex = 0;
+
+// Function to update the content based on the current post index
+function updateContent() {
+  const currentPost = blogPosts[currentPostIndex];
+
+  document.getElementById('blog-title').textContent = currentPost.title;
+  document.getElementById('blog-description').innerHTML = currentPost.description;
+  document.getElementById('blog-number').textContent = (currentPostIndex + 1).toString().padStart(2, '0');
+  document.querySelector('.image img').src = currentPost.imagePath;
+
+  const readMoreButton = document.getElementById('read-more-btn');
+  readMoreButton.href = currentPost.link;
 }
 
-// Delay the start of typing by the specified delay
-setTimeout(startTyping, delay);
+// Function to navigate to the next blog post
+function nextBlogPost() {
+  currentPostIndex = (currentPostIndex + 1) % blogPosts.length;
+  updateContent();
+}
 
-//ANOTHER TYPING ANIMATION
-var typed = new Typed('#auto-type1', {
-    strings: ['Welcome to my site!'],
-    typeSpeed: 50,
-    loop: false,
+// Function to navigate to the previous blog post
+function previousBlogPost() {
+  currentPostIndex = (currentPostIndex - 1 + blogPosts.length) % blogPosts.length;
+  updateContent();
+}
+
+// Event listeners for the navigation buttons
+document.getElementById('next-btn').addEventListener('click', nextBlogPost);
+document.getElementById('previous-btn').addEventListener('click', previousBlogPost);
+
+// Initially update the content with the first blog post
+updateContent();
+
+
+
+// Project Section Functions
+document.addEventListener("DOMContentLoaded", function() {
+  const carousel = document.querySelector(".carousel");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  const dotIndicators = document.querySelector(".dot-indicators");
+
+  prevButton.addEventListener("click", function() {
+    carousel.scrollBy({
+      left: -carousel.offsetWidth,
+      behavior: "smooth"
+    });
+  });
+
+  nextButton.addEventListener("click", function() {
+    carousel.scrollBy({
+      left: carousel.offsetWidth,
+      behavior: "smooth"
+    });
+  });
+
+  carousel.addEventListener("scroll", function() {
+    const slideIndex = Math.round(carousel.scrollLeft / carousel.offsetWidth);
+    updateDotIndicators(slideIndex);
+  });
+
+  function updateDotIndicators(currentIndex) {
+    const slides = carousel.getElementsByClassName("slide");
+    const dotIndicatorContainer = document.querySelector(".dot-indicators");
+
+    dotIndicatorContainer.innerHTML = "";
+
+    for (let i = 0; i < slides.length; i++) {
+      const dot = document.createElement("span");
+      dot.classList.add("dot-indicator");
+      if (i === currentIndex) {
+        dot.classList.add("active-dot");
+      }
+      dot.addEventListener("click", function() {
+        carousel.scroll({
+          left: i * carousel.offsetWidth,
+          behavior: "smooth"
+        });
+      });
+      dotIndicatorContainer.appendChild(dot);
+    }
+  }
 });
+
